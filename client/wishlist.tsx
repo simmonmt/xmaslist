d925987e5c3f344451f9e20e5141ddecc7cd48b9
Matthew Simmons <simmonmt@acm.org>
2021-01-03 17:44:01 -0500

    bring in material styles

--- client/wishlist.tsx
+++ client/wishlist.tsx
@@ -1,8 +1,12 @@
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { green } from '@material-ui/core/colors';
+import { createStyles, withStyles } from '@material-ui/core/styles';
 import { ItemModel } from './model';
 import * as React from 'react';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 interface Props {
+    classes: any;
 };
 
 interface State {
@@ -10,7 +14,7 @@ interface State {
     ids: string[];
 };
 
-export class WishList extends React.Component<Props, State> {
+class WishList extends React.Component<Props, State> {
     private readonly model: ItemModel;
 
     constructor(props) {
@@ -40,11 +44,10 @@ export class WishList extends React.Component<Props, State> {
 
     render() {
         return (
-		//<div className={this.props.classes.root}>
-		<div>
+	    <div className={this.props.classes.root}>
                 <h1>Wish List</h1>
 		{this.state.loading && <LinearProgress />}
-		</div>
+	    </div>
 
             // {this.state.ids.map((id) => (
             //         <ListItemComponent
@@ -58,3 +61,37 @@ export class WishList extends React.Component<Props, State> {
         )
     }
 }
+
+const styles = (theme: Theme) => createStyles({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+    chip: {
+        margin: theme.spacing(0.5),
+    },
+    headingColumn: {
+        flexBasis: '100%',
+    },
+    buttonSuccess: {
+        backgroundColor: green[500],
+        '&:hover': {
+            backgroundColor: green[700],
+        },
+    },
+    buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
+});
+
+const StyledWishList = withStyles(styles)(WishList);
+
+export { StyledWishList as WishList };
