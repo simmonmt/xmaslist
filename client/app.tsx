35d5ff494d4fa0d9857fd8542294a51e2d71fbce
Matthew Simmons <simmonmt@acm.org>
2021-01-04 14:22:58 -0500

    initial router setup

--- client/app.tsx
+++ client/app.tsx
@@ -1,12 +1,37 @@
 import * as React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { WishList } from "./wishlist";
 
 function App() {
   return (
-    <div className="App">
-      <WishList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/view">View</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/view">
+            <WishList />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
