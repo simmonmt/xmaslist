5642768a6583195f1d238ec3a233b212db44d039
Matthew Simmons <simmonmt@acm.org>
2021-04-25 19:37:20 -0400

    Start grpc server

--- server/main.go
+++ server/main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	uspb "github.com/simmonmt/xmaslist/proto/user_service"
 )
 
-func main() {
-	fmt.Println("Hello world")
+var (
+	port = flag.Int("port", -1, "port to use")
+)
 
-	fmt.Println(grpc.Version)
+type userServer struct{}
+
+func (s *userServer) Login(ctx context.Context, req *uspb.LoginRequest) (*uspb.LoginResponse, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *userServer) Logout(ctx context.Context, req *uspb.LogoutRequest) (*uspb.LogoutResponse, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func newServer() *userServer {
+	s := &userServer{}
+	return s
+}
+
+func main() {
+	flag.Parse()
+
+	if *port == -1 {
+		log.Fatalf("--port is required")
+	}
+
+	sock, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{}
+	server := grpc.NewServer(opts...)
+	uspb.RegisterUserServiceServer(server, newServer())
+	reflection.Register(server)
+
+	log.Printf("serving on port %v...\n", *port)
+	if err := server.Serve(sock); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
