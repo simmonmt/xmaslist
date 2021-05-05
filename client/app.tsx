879896e881e8650f204b486e640c23c00bb599ca
Matthew Simmons <simmonmt@acm.org>
2021-05-05 08:40:13 -0400

    turn app into a class

--- client/app.tsx
+++ client/app.tsx
@@ -3,38 +3,44 @@ import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { Login } from "./login";
 import { WishList } from "./wishlist";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/view">View</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+interface Props {}
 
-        <Switch>
-          <Route path="/view">
-            <WishList />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+interface State {}
+
+class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/view">View</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/view">
+              <WishList />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 function Home() {
