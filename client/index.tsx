cd65326442adc7782429d9f59c1d78e470616ecd
Matthew Simmons <simmonmt@acm.org>
2021-05-07 08:54:47 -0400

    remove login from index
    it was just there as a demo

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { UserServiceClient } from "../proto/user_service_grpc_web_pb";
-import { LoginRequest } from "../proto/user_service_pb";
-import App from "./app";
+import { App } from "./app";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,22 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-const userService = new UserServiceClient(
-  "http://nash.simmonmt.org:8081",
-  null,
-  null
-);
-
-let req = new LoginRequest();
-req.setUsername("simmonmt");
-req.setPassword("hunter2");
-
-userService.login(req, undefined, function (err, response) {
-  if (err) {
-    console.log(err.code);
-    console.log(err.message);
-  } else {
-    console.log(response);
-  }
-});
