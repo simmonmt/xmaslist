a4d0f501485085bad70ce452783af5ba89511749
Matthew Simmons <simmonmt@acm.org>
2021-05-08 17:58:45 -0400

    add the fwd-compat unimplemented server; use status

--- backend/listservice/list_service.go
+++ backend/listservice/list_service.go
@@ -2,7 +2,6 @@ package listservice
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/simmonmt/xmaslist/backend/database"
 	"github.com/simmonmt/xmaslist/backend/sessions"
@@ -10,44 +9,48 @@ import (
 	"google.golang.org/grpc"
 
 	lspb "github.com/simmonmt/xmaslist/proto/list_service"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 )
 
 type listServer struct {
+	lspb.UnimplementedListServiceServer
+
 	clock          util.Clock
 	sessionManager *sessions.Manager
 	db             *database.DB
 }
 
 func (s *listServer) GetLists(ctx context.Context, req *lspb.GetListsRequest) (*lspb.GetListsResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) NewList(ctx context.Context, req *lspb.NewListRequest) (*lspb.NewListResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) DeleteList(ctx context.Context, req *lspb.DeleteListRequest) (*lspb.DeleteListResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) UpdateList(ctx context.Context, req *lspb.UpdateListRequest) (*lspb.UpdateListResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) GetListItems(ctx context.Context, req *lspb.GetListItemsRequest) (*lspb.GetListItemsResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) AddListItem(ctx context.Context, req *lspb.AddListItemRequest) (*lspb.AddListItemResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) DeleteListItem(ctx context.Context, req *lspb.DeleteListItemRequest) (*lspb.DeleteListItemResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func (s *listServer) UpdateListItem(ctx context.Context, req *lspb.UpdateListItemRequest) (*lspb.UpdateListItemResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
 func RegisterHandlers(server *grpc.Server, clock util.Clock, sessionManager *sessions.Manager, db *database.DB) {
