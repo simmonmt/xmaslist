456cae0d714b1ef19fdb07384e7da966ece2e008
Matthew Simmons <simmonmt@acm.org>
2021-06-08 14:10:55 -0400

    Add show/hide archived switch

--- client/home.tsx
+++ client/home.tsx
@@ -1,4 +1,5 @@
 import Fab from "@material-ui/core/Fab";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import IconButton from "@material-ui/core/IconButton";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import List from "@material-ui/core/List";
@@ -7,6 +8,7 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import Switch from "@material-ui/core/Switch";
 import AddIcon from "@material-ui/icons/Add";
 import ArchiveIcon from "@material-ui/icons/Archive";
 import UnarchiveIcon from "@material-ui/icons/Unarchive";
@@ -30,6 +32,7 @@ interface State {
   loggedIn: boolean;
   errorMessage: string;
   lists: ListProto[];
+  showArchived: boolean;
 }
 
 class Home extends React.Component<Props, State> {
@@ -40,16 +43,24 @@ class Home extends React.Component<Props, State> {
       loggedIn: true,
       errorMessage: "",
       lists: [],
+      showArchived: false,
     };
 
     this.handleAlertClose = this.handleAlertClose.bind(this);
+    this.handleShowArchivedChange = this.handleShowArchivedChange.bind(this);
   }
 
   componentDidMount() {
+    this.loadLists();
+  }
+
+  private loadLists() {
     let gotLists: ListProto[] = [];
 
+    this.setState({ lists: [], errorMessage: "" });
+
     this.props.listModel
-      .listLists(false)
+      .listLists(this.state.showArchived)
       .then((lists: ListProto[]) => {
         console.log("got lists", lists);
         let needIds = new Set<number>();
@@ -66,7 +77,6 @@ class Home extends React.Component<Props, State> {
         this.setState({
           loading: false,
           lists: gotLists,
-          errorMessage: "no error",
         });
       })
       .catch((status: Status) => {
@@ -96,6 +106,19 @@ class Home extends React.Component<Props, State> {
             {this.state.errorMessage}
           </Alert>
         )}
+        <div className={this.props.classes.switchDiv}>
+          <FormControlLabel
+            label="Show archived lists"
+            control={
+              <Switch
+                checked={this.state.showArchived}
+                onChange={this.handleShowArchivedChange}
+                name="showArchived"
+              />
+            }
+            labelPlacement="start"
+          />
+        </div>
         <List>{this.state.lists.map((list) => this.listElement(list))}</List>
         <Fab
           color="primary"
@@ -152,6 +175,10 @@ class Home extends React.Component<Props, State> {
   private handleAlertClose() {
     this.setState({ errorMessage: "" });
   }
+
+  private handleShowArchivedChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ showArchived: evt.target.checked }, this.loadLists);
+  }
 }
 
 const homeStyles = (theme: Theme) =>
@@ -163,6 +190,10 @@ const homeStyles = (theme: Theme) =>
       position: "absolute",
       bottom: theme.spacing(2),
       right: theme.spacing(2),
+    },
+    switchDiv: {
+      display: "flex",
+      justifyContent: "flex-end",
     },
   });
 
