eb678fff2b9ae11cf1f3ab9f7f8c06085f7939bb
Matthew Simmons <simmonmt@acm.org>
2021-06-11 22:25:56 -0400

    add metadata for view list

--- client/view_list.tsx
+++ client/view_list.tsx
@@ -1,11 +1,14 @@
+import Card from "@material-ui/core/Card";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import { createStyles, withStyles } from "@material-ui/core/styles";
-import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import Typography from "@material-ui/core/Typography";
 import Alert from "@material-ui/lab/Alert";
+import { format as formatDate } from "date-fns";
+import { Status, StatusCode } from "grpc-web";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import { Link, Redirect, withRouter } from "react-router-dom";
+import { List as ListProto } from "../proto/list_pb";
 import { ListModel } from "./list_model";
 import { User } from "./user";
 
@@ -23,16 +26,26 @@ interface State {
   loggedIn: boolean;
   loading: boolean;
   errorMessage: string;
+  list: ListProto | null;
 }
 
 class ViewList extends React.Component<Props, State> {
+  private readonly listId: string;
+
   constructor(props: Props) {
     super(props);
     this.state = {
       loggedIn: true,
       loading: false,
       errorMessage: "",
+      list: null,
     };
+
+    this.listId = this.props.match.params.listId;
+  }
+
+  componentDidMount() {
+    this.loadData();
   }
 
   render() {
@@ -55,11 +68,30 @@ class ViewList extends React.Component<Props, State> {
           </Typography>
         </div>
 
-        <div>
-          <Typography variant="body1">
-            View list {this.props.match.params.listId}
-          </Typography>
-        </div>
+        <Card className={this.props.classes.card} raised>
+          {this.listMeta()}
+        </Card>
+
+        <Card className={this.props.classes.card} raised>
+          elements
+        </Card>
+      </div>
+    );
+  }
+
+  private listMeta() {
+    if (!this.state.list) return;
+
+    const data = this.state.list.getData()!;
+    return (
+      <div className={this.props.classes.meta}>
+        <Typography variant="h2">{data.getName()}</Typography>
+        <Typography variant="body1" color="textSecondary">
+          {
+            // Long localized date
+            formatDate(data.getEventDate(), "PPPP")
+          }
+        </Typography>
       </div>
     );
   }
@@ -67,12 +99,39 @@ class ViewList extends React.Component<Props, State> {
   private handleAlertClose() {
     this.setState({ errorMessage: "" });
   }
+
+  private loadData() {
+    this.setState({ loading: true }, () => {
+      this.props.listModel
+        .getList(this.listId)
+        .then((list: ListProto) => {
+          this.setState({ loading: false, list: list });
+        })
+        .catch((status: Status) => {
+          if (status.code === StatusCode.UNAUTHENTICATED) {
+            this.setState({ loggedIn: false });
+            return;
+          }
+
+          this.setState({
+            loading: false,
+            errorMessage: status.details,
+          });
+        });
+    });
+  }
 }
 
-const viewListStyles = (theme: Theme) =>
+const viewListStyles = () =>
   createStyles({
     root: {
       width: "100%",
+    },
+    card: {
+      margin: "1em",
+    },
+    meta: {
+      textAlign: "center",
     },
   });
 
