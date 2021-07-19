f23f7cc2e085fac980e3ff2e5df677f6994959ed
Matthew Simmons <simmonmt@acm.org>
2021-07-19 11:56:18 -0400

    add create list item dialog, wire it up

---
+++ client/create_list_item_dialog.tsx
@@ -0,0 +1,166 @@
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import * as React from "react";
+import { ListItemData as ListItemDataProto } from "../proto/list_item_pb";
+
+function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+interface Props {
+  classes: any;
+  open: boolean;
+  onClose: (listData: ListItemDataProto | null) => void;
+}
+
+interface State {
+  name: string;
+  nameErrorMessage: string;
+  desc: string;
+  descErrorMessage: string;
+  url: string | null;
+  urlErrorMessage: string;
+}
+
+class CreateListItemDialog extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      name: "",
+      nameErrorMessage: "",
+      desc: "",
+      descErrorMessage: "",
+      url: "",
+      urlErrorMessage: "",
+    };
+
+    this.onCancelClicked = this.onCancelClicked.bind(this);
+    this.onOkClicked = this.onOkClicked.bind(this);
+  }
+
+  render() {
+    const onNameChange = (name: string) => {
+      this.setState({ name: name });
+    };
+
+    const onDescChange = (desc: string) => {
+      this.setState({ desc: desc });
+    };
+
+    const onUrlChange = (url: string) => {
+      this.setState({ url: url });
+    };
+
+    return (
+      <Dialog
+        open={this.props.open}
+        onClose={this.onCancelClicked}
+        aria-labelledby="create-item-dialog-title"
+      >
+        <DialogTitle id="create-item-dialog-title">
+          Create List Item
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            label="Item name"
+            fullWidth
+            value={this.state.name}
+            onChange={(event) => onNameChange(event.target.value)}
+            error={this.state.nameErrorMessage.length > 0}
+            helperText={this.state.nameErrorMessage}
+          />
+          <TextField
+            margin="dense"
+            id="desc"
+            label="Description"
+            fullWidth
+            value={this.state.desc}
+            onChange={(event) => onDescChange(event.target.value)}
+            error={this.state.descErrorMessage.length > 0}
+            helperText={this.state.descErrorMessage}
+          />
+          <TextField
+            margin="dense"
+            id="url"
+            label="URL"
+            fullWidth
+            value={this.state.url}
+            onChange={(event) => onUrlChange(event.target.value)}
+            error={this.state.urlErrorMessage.length > 0}
+            helperText={this.state.urlErrorMessage}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.onCancelClicked} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.onOkClicked} color="primary">
+            Create List
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
+  private onCancelClicked() {
+    this.props.onClose(null);
+  }
+
+  private onOkClicked() {
+    let error = false;
+
+    if (this.state.name) {
+      this.setState({ nameErrorMessage: "" });
+    } else {
+      this.setState({
+        nameErrorMessage: "This field is required",
+      });
+      error = true;
+    }
+
+    if (this.state.url) {
+      if (isValidUrl(this.state.url)) {
+        this.setState({ urlErrorMessage: "" });
+      } else {
+        this.setState({ urlErrorMessage: "Not a valid URL" });
+        error = true;
+      }
+    }
+
+    if (error) {
+      return;
+    }
+
+    const itemData = new ListItemDataProto();
+    itemData.setName(this.state.name);
+    if (this.state.desc) {
+      itemData.setDesc(this.state.desc);
+    }
+    if (this.state.url) {
+      itemData.setUrl(this.state.url);
+    }
+    this.props.onClose(itemData);
+  }
+}
+
+const createListItemDialogStyles = () => createStyles({});
+
+const exportCreateListItemDialog: any = withStyles(createListItemDialogStyles)(
+  CreateListItemDialog
+);
+
+export { exportCreateListItemDialog as CreateListItemDialog };
--- client/edit_list.tsx
+++ client/edit_list.tsx
@@ -1,31 +1,85 @@
+import Fab from "@material-ui/core/Fab";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import { Theme } from "@material-ui/core/styles/createTheme";
+import AddIcon from "@material-ui/icons/Add";
 import * as React from "react";
 import { withRouter } from "react-router-dom";
+import { ListItemData as ListItemDataProto } from "../proto/list_item_pb";
+import { CreateListItemDialog } from "./create_list_item_dialog";
 import { ItemList, ItemListApiArgs, Props as ItemListProps } from "./item_list";
 import { ItemListElement } from "./item_list_element";
 
-interface Props extends ItemListProps {}
+interface Props extends ItemListProps {
+  classes: any;
+}
 
-interface State {}
+interface State {
+  createItemDialogOpen: boolean;
+}
 
 class EditList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      createItemDialogOpen: false,
+    };
+
+    this.onCreateClicked = this.onCreateClicked.bind(this);
+    this.onCreateItemDialogClose = this.onCreateItemDialogClose.bind(this);
+  }
+
   render() {
     return (
-      <ItemList {...this.props}>
-        {({ key, item, currentUser, itemUpdater }: ItemListApiArgs) => (
-          <ItemListElement
-            key={key}
-            showClaim={false}
-            mutable={true}
-            item={item}
-            itemUpdater={itemUpdater}
-            currentUser={currentUser}
-          />
-        )}
-      </ItemList>
+      <React.Fragment>
+        <ItemList {...this.props}>
+          {({ key, item, currentUser, itemUpdater }: ItemListApiArgs) => (
+            <ItemListElement
+              key={key}
+              showClaim={false}
+              mutable={true}
+              item={item}
+              itemUpdater={itemUpdater}
+              currentUser={currentUser}
+            />
+          )}
+        </ItemList>
+        <Fab
+          color="primary"
+          aria-label="create"
+          className={this.props.classes.fab}
+          onClick={this.onCreateClicked}
+        >
+          <AddIcon />
+        </Fab>
+        <CreateListItemDialog
+          open={this.state.createItemDialogOpen}
+          onClose={this.onCreateItemDialogClose}
+        />
+      </React.Fragment>
     );
+  }
+
+  private onCreateClicked() {
+    this.setState({ createItemDialogOpen: true });
+  }
+
+  private onCreateItemDialogClose(itemData: ListItemDataProto | null) {
+    this.setState({ createItemDialogOpen: false });
+    if (!itemData) {
+      return;
+    }
   }
 }
 
-const exportEditList: any = withRouter(EditList);
+const editListStyles = (theme: Theme) =>
+  createStyles({
+    fab: {
+      position: "absolute",
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  });
+
+const exportEditList: any = withStyles(editListStyles)(withRouter(EditList));
 
 export { exportEditList as EditList };
