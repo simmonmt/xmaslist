f6b80831e9ae560ba141e619927485ee6183ba64
Matthew Simmons <simmonmt@acm.org>
2021-07-25 18:48:22 -0400

    split ListListElement into its own class

--- client/home.tsx
+++ client/home.tsx
@@ -5,164 +5,22 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import Divider from "@material-ui/core/Divider";
 import Fab from "@material-ui/core/Fab";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import IconButton from "@material-ui/core/IconButton";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { Theme } from "@material-ui/core/styles/createTheme";
 import Switch from "@material-ui/core/Switch";
-import Typography from "@material-ui/core/Typography";
 import AddIcon from "@material-ui/icons/Add";
-import DeleteIcon from "@material-ui/icons/Delete";
-import RestoreFromTrashIcon from "@material-ui/icons/RestoreFromTrash";
 import Alert from "@material-ui/lab/Alert";
-import { format } from "date-fns";
 import { Error as GrpcError, StatusCode } from "grpc-web";
 import * as React from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { List as ListProto, ListData as ListDataProto } from "../proto/list_pb";
 import { EditListDialog } from "./edit_list_dialog";
+import { ListListElement } from "./list_list_element";
 import { ListModel } from "./list_model";
 import { User } from "./user";
 import { UserModel } from "./user_model";
-
-interface ListItemLinkProps {
-  to: string;
-  children: React.ReactNode;
-}
-
-function ListItemLink(props: ListItemLinkProps) {
-  const { to, children } = props;
-
-  // I'm not entirely sure what this does. Source:
-  // https://material-ui.com/guides/composition/#wrapping-components
-  //
-  // useMemo makes CustomLink a static component -- it only gets re-rendered
-  // when 'to' changes. So I guess that means it won't change when just children
-  // change? The ref lets link access the anchor created by ListItem. I think.
-  const CustomLink = React.useMemo(
-    () =>
-      React.forwardRef<HTMLAnchorElement>((linkProps, ref) => (
-        <Link ref={ref} to={to} {...linkProps} />
-      )),
-    [to]
-  );
-
-  return (
-    <ListItem button component={CustomLink}>
-      {children}
-    </ListItem>
-  );
-}
-
-interface ListElementProps {
-  list: ListProto;
-  userModel: UserModel;
-  currentUser: User;
-  curYear: number;
-  onDeleteClicked: (id: string, isActive: boolean) => void;
-  classes: any;
-}
-
-interface ListElementState {}
-
-class ListElement extends React.Component<ListElementProps, ListElementState> {
-  render() {
-    const list = this.props.list;
-    const listId = list.getId();
-    const data = list.getData();
-    const meta = list.getMetadata();
-    if (!listId || !data || !meta) {
-      return <div />;
-    }
-
-    let eventDate = new Date(data.getEventDate() * 1000);
-    const monthStr = format(eventDate, "MMM");
-    const day = eventDate.getDate();
-    const year = eventDate.getFullYear();
-
-    const ownerUser = this.props.userModel.getUser(meta.getOwner());
-    const owner = ownerUser ? ownerUser.fullname : "unknown";
-
-    const linkVerb =
-      this.props.currentUser.id === meta.getOwner() ? "edit" : "view";
-    const linkTarget = `/${linkVerb}/${listId}`;
-
-    return (
-      <ListItemLink to={linkTarget}>
-        <ListItemText>
-          <div className={this.props.classes.listItem}>
-            <div className={this.props.classes.listDate}>
-              <Typography variant="body1" color="textSecondary">
-                {this.props.curYear == year ? (
-                  <span>
-                    {monthStr} {day}
-                  </span>
-                ) : (
-                  <span>
-                    {monthStr} {day}
-                    <br />
-                    {year}
-                  </span>
-                )}
-              </Typography>
-            </div>
-            <div>
-              <div>{data.getName()}</div>
-              <div></div>
-            </div>
-            <div className={this.props.classes.listGrow} />
-            <div className={this.props.classes.listMeta}>
-              <div>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="span"
-                >
-                  For:
-                </Typography>{" "}
-                {data.getBeneficiary()}
-              </div>
-              <div>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="span"
-                >
-                  By:
-                </Typography>{" "}
-                {owner}
-              </div>
-            </div>
-          </div>
-        </ListItemText>
-        {this.props.currentUser.isAdmin && (
-          <ListItemSecondaryAction>
-            {this.deleteButton(list.getId(), meta.getActive())}
-          </ListItemSecondaryAction>
-        )}
-      </ListItemLink>
-    );
-  }
-
-  private deleteButton(id: string, isActive: boolean) {
-    const label = isActive ? "delete" : "undelete";
-    const icon = isActive ? <DeleteIcon /> : <RestoreFromTrashIcon />;
-
-    return (
-      <IconButton
-        edge="end"
-        aria-label={label}
-        onClick={() => this.props.onDeleteClicked(id, isActive)}
-      >
-        {icon}
-      </IconButton>
-    );
-  }
-}
 
 interface HomeProps {
   listModel: ListModel;
@@ -211,10 +69,9 @@ class Home extends React.Component<HomeProps, HomeState> {
   }
 
   private loadLists() {
-    let gotLists: ListProto[] = [];
-
     this.setState({ lists: [], errorMessage: "" });
 
+    let loadedLists: ListProto[] = [];
     this.props.listModel
       .listLists(this.state.showDeleted)
       .then((lists: ListProto[]) => {
@@ -223,13 +80,13 @@ class Home extends React.Component<HomeProps, HomeState> {
           needIds.add(list.getMetadata()!.getOwner());
         }
 
-        gotLists = lists;
+        loadedLists = lists;
         return this.props.userModel.loadUsers(Array.from(needIds.values()));
       })
       .then(() => {
         this.setState({
           loading: false,
-          lists: gotLists,
+          lists: loadedLists,
         });
       })
       .catch((error: GrpcError) => {
@@ -311,14 +168,13 @@ class Home extends React.Component<HomeProps, HomeState> {
 
       const list = lists[i];
       out.push(
-        <ListElement
+        <ListListElement
           key={list.getId()}
           list={list}
           userModel={this.props.userModel}
           currentUser={this.props.currentUser}
           curYear={this.curYear}
           onDeleteClicked={this.handleDeleteClicked}
-          classes={this.props.classes}
         />
       );
     }
@@ -412,22 +268,6 @@ const homeStyles = (theme: Theme) =>
     switchDiv: {
       display: "flex",
       justifyContent: "flex-end",
-    },
-    listDate: {
-      textAlign: "center",
-      width: "10ch",
-    },
-    listGrow: {
-      flexGrow: 1,
-    },
-    listItem: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    listMeta: {
-      textAlign: "right",
-      marginRight: "3ch",
     },
   });
 
---
+++ client/list_list_element.tsx
@@ -0,0 +1,178 @@
+import IconButton from "@material-ui/core/IconButton";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/Delete";
+import RestoreFromTrashIcon from "@material-ui/icons/RestoreFromTrash";
+import { format } from "date-fns";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { List as ListProto } from "../proto/list_pb";
+import { User } from "./user";
+import { UserModel } from "./user_model";
+
+interface ListItemLinkProps {
+  to: string;
+  children: React.ReactNode;
+}
+
+function ListItemLink(props: ListItemLinkProps) {
+  const { to, children } = props;
+
+  // I'm not entirely sure what this does. Source:
+  // https://material-ui.com/guides/composition/#wrapping-components
+  //
+  // useMemo makes CustomLink a static component -- it only gets re-rendered
+  // when 'to' changes. So I guess that means it won't change when just children
+  // change? The ref lets link access the anchor created by ListItem. I think.
+  const CustomLink = React.useMemo(
+    () =>
+      React.forwardRef<HTMLAnchorElement>((linkProps, ref) => (
+        <Link ref={ref} to={to} {...linkProps} />
+      )),
+    [to]
+  );
+
+  return (
+    <ListItem button component={CustomLink}>
+      {children}
+    </ListItem>
+  );
+}
+
+interface ListListElementProps {
+  list: ListProto;
+  userModel: UserModel;
+  currentUser: User;
+  curYear: number;
+  onDeleteClicked: (id: string, isActive: boolean) => void;
+  classes: any;
+}
+
+interface ListListElementState {}
+
+class ListListElement extends React.Component<
+  ListListElementProps,
+  ListListElementState
+> {
+  render() {
+    const list = this.props.list;
+    const listId = list.getId();
+    const data = list.getData();
+    const meta = list.getMetadata();
+    if (!listId || !data || !meta) {
+      return <div />;
+    }
+
+    let eventDate = new Date(data.getEventDate() * 1000);
+    const monthStr = format(eventDate, "MMM");
+    const day = eventDate.getDate();
+    const year = eventDate.getFullYear();
+
+    const ownerUser = this.props.userModel.getUser(meta.getOwner());
+    const owner = ownerUser ? ownerUser.fullname : "unknown";
+
+    const linkVerb =
+      this.props.currentUser.id === meta.getOwner() ? "edit" : "view";
+    const linkTarget = `/${linkVerb}/${listId}`;
+
+    return (
+      <ListItemLink to={linkTarget}>
+        <ListItemText>
+          <div className={this.props.classes.listItem}>
+            <div className={this.props.classes.listDate}>
+              <Typography variant="body1" color="textSecondary">
+                {this.props.curYear == year ? (
+                  <span>
+                    {monthStr} {day}
+                  </span>
+                ) : (
+                  <span>
+                    {monthStr} {day}
+                    <br />
+                    {year}
+                  </span>
+                )}
+              </Typography>
+            </div>
+            <div>
+              <div>{data.getName()}</div>
+              <div></div>
+            </div>
+            <div className={this.props.classes.listGrow} />
+            <div className={this.props.classes.listMeta}>
+              <div>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="span"
+                >
+                  For:
+                </Typography>{" "}
+                {data.getBeneficiary()}
+              </div>
+              <div>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="span"
+                >
+                  By:
+                </Typography>{" "}
+                {owner}
+              </div>
+            </div>
+          </div>
+        </ListItemText>
+        {this.props.currentUser.isAdmin && (
+          <ListItemSecondaryAction>
+            {this.deleteButton(list.getId(), meta.getActive())}
+          </ListItemSecondaryAction>
+        )}
+      </ListItemLink>
+    );
+  }
+
+  private deleteButton(id: string, isActive: boolean) {
+    const label = isActive ? "delete" : "undelete";
+    const icon = isActive ? <DeleteIcon /> : <RestoreFromTrashIcon />;
+
+    return (
+      <IconButton
+        edge="end"
+        aria-label={label}
+        onClick={() => this.props.onDeleteClicked(id, isActive)}
+      >
+        {icon}
+      </IconButton>
+    );
+  }
+}
+
+const listListElementStyles = () =>
+  createStyles({
+    listDate: {
+      textAlign: "center",
+      width: "10ch",
+    },
+    listGrow: {
+      flexGrow: 1,
+    },
+    listItem: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    listMeta: {
+      textAlign: "right",
+      marginRight: "3ch",
+    },
+  });
+
+const exportListListElement: any = withStyles(listListElementStyles)(
+  ListListElement
+);
+
+export { exportListListElement as ListListElement };
